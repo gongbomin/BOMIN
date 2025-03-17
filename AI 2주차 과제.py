0bd2b4334a8bbfac695aefc2324e99ca1802f43f
gongbomin <gongbomin@naver.com>
2025-03-17 10:46:26 +0900

    AI 2주차 과제.py

---
+++ AI 2주차 과제.py
@@ -0,0 +1,35 @@
+#LAB 2-1
+print('나의 이름은:', '홍길동')
+print('나의 나이는:', 27)
+print('나의 키는:',179,'cm입니다.')
+print('10+20=', 10+20)
+print('10*20=', 10*20)
+
+#LAB 2-4
+name= '전우치'
+print('나의 이름은:', name)
+age= 27
+print('나의 나이는:', age)
+height= 179
+print('나의 키는:',height,'cm입니다.')
+sum = 10 + 20
+print('10+20=', sum)
+mult = 10 * 20
+print('10*20=', mult)
+
+#LAB 2-6
+width = 20
+height = 40
+width = 30
+area = width * height
+print('사각형의 면적', area)
+
+#LAB 2-7
+print('1-1)', 123 * 456)
+print('1-2)', 1357 + 2468)
+print('1-3)', 5 **4)
+print('1-4)', 10/4)
+print('1-5)', 10//5)
+print('1-6)', 10%5)
+print('2.', 5 % 2)
+print('3.', 2 ** 0.5 , 3 ** 0.5)
